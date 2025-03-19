da30cf3187b8413796489d63188616e37a987713
Gian Scherer <791e@proton.me>
2025-03-19 12:09:18 +0100

    Create blinky program, not yet tested

--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -2,25 +2,23 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-use log::info;
+use esp_hal::{
+    clock::CpuClock,
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
+    main,
+};
 
 #[main]
 fn main() -> ! {
-    // generator version: 0.3.1
-
-    esp_println::logger::init_logger_from_env();
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+    let delay = Delay::new();
+
+    let mut led = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        led.toggle();
+        delay.delay_millis(1000);
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
